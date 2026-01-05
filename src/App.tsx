8173a3f799aa1f7a9b8a917418ef379694387d1c
laszlocaballero <laszlocaballero@gmail.com>
2026-01-04 23:56:48 -0500

    feat: add sidebar layout and configuration, integrate clsx and tailwind-merge utilities

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { Route, Routes } from "react-router";
 import AuthRoutes from "./routes/auth/Auth.routes";
 import DashboardRoutes from "./routes/dashboard/Dashboard.routes";
+import Layout from "./components/layouts/layout";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/auth/*" element={<AuthRoutes />} />
-      <Route path="/*" element={<DashboardRoutes />}/>
+      <Route
+        path="/*"
+        element={
+          <Layout>
+            <DashboardRoutes />
+          </Layout>
+        }
+      />
     </Routes>
   );
 }
