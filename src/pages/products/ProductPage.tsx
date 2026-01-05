b31b42eafb1e95d6a6d299fc4f0093e604e7c2f9
ManuL0129 <manumonla@hotmai.com>
2026-01-05 00:01:32 -0500

    refactor: improve code formatting and structure in ProductPage component

--- src/pages/products/ProductPage.tsx
+++ src/pages/products/ProductPage.tsx
@@ -1,12 +1,23 @@
 import { useAuth } from "@/context/AuthContext";
-import type { ApiResponse, ResponseCategories } from "@/interface/response.interface";
+import type {
+  ApiResponse,
+  ResponseCategories,
+} from "@/interface/response.interface";
 import { instance } from "@/libs/axios";
 import { useQuery } from "@tanstack/react-query";
-import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@heroui/react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Divider,
+  Link,
+  Image,
+} from "@heroui/react";
 import { MdInventory } from "react-icons/md";
 
-
 export default function ProductPage() {
+  const condicion = true;
   const { token } = useAuth();
   const { data, isLoading } = useQuery<ApiResponse<ResponseCategories[]>>({
     queryKey: ["products"],
@@ -24,24 +35,30 @@ export default function ProductPage() {
     return <h1>Cargando</h1>;
   }
 
-  return <div>{
+  return (
+    <div>
+      {
         <div>
-            <Card className="max-w-[400px]">
+          <Card className="max-w-[400px]">
             <CardHeader className="flex gap-3">
-            <MdInventory />
-                <p className="text-md font-bold">Agua</p>
+              <MdInventory />
+              <p className="text-md font-bold">Agua</p>
             </CardHeader>
             <Divider />
             <CardBody>
-                <p>
-                    Recipiente portátil diseñado para contener líquidos.
-                </p>
+              <p>Recipiente portátil diseñado para contener líquidos.</p>
             </CardBody>
             <Divider />
-            <CardFooter>
-                
+            <CardFooter
+              className={`font-semibold ${
+                condicion ? "text-green-600" : "text-red-600"
+              }`}
+            >
+              {condicion ? "Activo" : "Inactivo"}
             </CardFooter>
-            </Card>
+          </Card>
         </div>
-    }</div>;
+      }
+    </div>
+  );
 }
